3ae8a566cb0a971d90f03868c760214f9eefcaeb
saitarun135 <saitarun135@gm.com>
2021-03-26 13:40:46 +0530

    Ectending the program

--- main/java/FindMaxGeneric.java
+++ main/java/FindMaxGeneric.java
@@ -3,28 +3,40 @@
 
 public class FindMaxGeneric<T extends Comparable<T>> {
 
-    private final T a;
-    private final T b;
-    private final T c;
+    private final T a ;T b;T c;T p;T q;T r;   
+    
 
-    public FindMaxGeneric(T a, T b, T c) {
+    public FindMaxGeneric(T a, T b, T c,T p,T q,T r) {
         this.a =a;
         this.b = b;
         this.c = c;
+        this.p =p;
+        this.q = q;
+        this.r = r;
+        
     }
 
     public T findMaximum() {
-        return FindMaxGeneric.findMaximum(a, b, c);
+        return FindMaxGeneric.findMaximum(a, b, c,p ,q ,r);
     }
 
     //Single generic method to compare different types of data
-    public static <T extends Comparable> T findMaximum(T a, T b, T c) {
+    public static <T extends Comparable> T findMaximum(T a, T b, T c,T p,T q,T r) {
         T max = a;
         if (b.compareTo(max) > 0) {
             max = b;
         }
         if (c.compareTo(max) > 0) {
             max = c;
+        }
+        if (p.compareTo(max) > 0) {
+            max = p;
+        }
+        if (q.compareTo(max) > 0) {
+            max = q;
+        }
+        if (r.compareTo(max) > 0) {
+            max = r;
         }
         return max;
     }
--- test/java/FindMaxGenericTest.java
+++ test/java/FindMaxGenericTest.java
@@ -1,34 +1,38 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+//private static final FindMaxGeneric findMax = new FindMaxGeneric(null, null, null);
 
 public class FindMaxGenericTest {
- //private static final FindMaxGeneric findMax = new FindMaxGeneric(null, null, null);
 
 	private static FindMaxGeneric findMax;
 
 	    @Test
-	    public void whenGivenThreeNumbers_ShouldReturnMaximumInteger() {
-	        Integer i1 = 10, i2 = 25, i3 = 20;
-	        findMax = new FindMaxGeneric(i1, i2, i3);
-	        Object result = findMax.findMaximum();               //stored in obj
-	        Assertions.assertEquals(i2, result);
+	    public void whenGivenThreeNumbers_ShouldReturnMaximumInteger() 
+	    {
+	       Integer i1 = 10, i2 = 25, i3 = 20, i4=30, i5=40, i6=50;
+	       findMax = new FindMaxGeneric(i1, i2, i3, i4 ,i5, i6);
+	       Object result = findMax.findMaximum();                                    //stored in obj
+	       Assertions.assertEquals(i6, result);
 	    }
 	
 	    @Test
-	    public void whenGivenThreeFloatValues_ShouldReturnMaximumFloat() {	
-	    	Float f1=2.5f, f2=5.5f, f3=10.25f;
-	    	findMax = new FindMaxGeneric(f1,f2,f3);
-	    	Comparable result=findMax.findMaximum();            //Inorder to use object,i am using comparable type
-	    	Assertions.assertEquals(f3, result);
+	    public void whenGivenThreeFloatValues_ShouldReturnMaximumFloat()
+	    {	
+	    	Float f1=2.5f, f2=5.5f, f3=10.25f, f4=12.25f, f5=8.24f, f6=2.524f;
+	    	findMax = new FindMaxGeneric(f1, f2, f3, f4, f5, f6);
+	    	Comparable result=findMax.findMaximum();                                 //Inorder to use object,i am using comparable type
+	    	Assertions.assertEquals(f4, result);
 	    }
+	    
 	    @Test
-	    public void whenGivenThreeStrings_ShouldReturnMaximumString() {
-	        String s1="Promogranate",s2="Orange",s3="Gua";
-	        findMax=new FindMaxGeneric(s1,s2,s3);
-	        String result=(String) findMax.findMaximum();         //typecasting
-	        Assertions.assertEquals(s1,result);
+	    public void whenGivenThreeStrings_ShouldReturnMaximumString()
+	    {
+	        String s1="Grape",s2="Orange",s3="Gua",s4="Kiwi", s5="Promogranate",s6="";
+	        findMax=new FindMaxGeneric(s1, s2, s3, s4, s5, s6);
+	        String result=(String) findMax.findMaximum();                                  //typecasting
+	        Assertions.assertEquals(s5,result);
 	    }
 	   
-	}
+}
 	
 	
