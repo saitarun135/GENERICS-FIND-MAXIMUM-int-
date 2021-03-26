dfcb77a4690ca2f5c836cea6a3310d537b045ba7
saitarun135 <saitarun135@gm.com>
2021-03-26 13:21:42 +0530

    Refactoring code by using Generic class

--- main/java/FindMaxGeneric.java
+++ main/java/FindMaxGeneric.java
@@ -1,13 +1,30 @@
-  //compareTo -- boolean
+
 //https://deepeshdarshan.wordpress.com/2013/01/17/finding-max-value-using-java-generics/
 
-public class FindMaxGeneric{
-	public <T extends Comparable>T findMaximum(T[] numberArray) {
-        T max = numberArray[0];
-        for (T element : numberArray) {
-           if (element.compareTo(max) > 0) {
-                max = element;
-            }
+public class FindMaxGeneric<T extends Comparable<T>> {
+
+    private final T a;
+    private final T b;
+    private final T c;
+
+    public FindMaxGeneric(T a, T b, T c) {
+        this.a =a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public T findMaximum() {
+        return FindMaxGeneric.findMaximum(a, b, c);
+    }
+
+    //Single generic method to compare different types of data
+    public static <T extends Comparable> T findMaximum(T a, T b, T c) {
+        T max = a;
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
         }
         return max;
     }
